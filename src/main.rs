c3edf3d52d6c6d327251b99f6722590dc5a18ff7
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-02-17 22:37:20 +0900

    🦆

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use rust_book::{Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("SelectBox ({} x {})", self.width, self.height);
+        println!("Options:");
+        for option in &self.options {
+            println!("- {}", option);
+        }
+    }
+}
+
+struct Button {
+    width: u32,
+    height: u32,
+    on_click: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Button ({} x {})", self.width, self.height);
+        println!("{} fired", self.on_click);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 100,
+                options: vec![String::from("Yes"), String::from("Wow")],
+            }),
+            Box::new(Button {
+                width: 10,
+                height: 10,
+                on_click: String::from("submit"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
