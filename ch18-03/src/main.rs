38f4c60c1a4aa20849837ee7ca6b2d2b174af23d
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-02-23 10:07:11 +0900

    課題

--- ch18-03/src/main.rs
+++ ch18-03/src/main.rs
@@ -7,9 +7,14 @@ fn main() {
 
     let post = post.request_review();
 
+    let mut post = post.reject();
+    post.add_text("world");
+
+    let post = post.request_review();
+
     let post = post.approve();
 
-    assert_eq!("hello", post.content());
+    assert_eq!("helloworld", post.content());
 
     // assert_eq!("", post.content());
 
