20310dd15ec9add71e62cfb47509fdce163cc435
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-02-23 09:40:38 +0900

    complete

--- ch18-03/src/main.rs
+++ ch18-03/src/main.rs
@@ -1,3 +1,14 @@
+use ch18_03::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("hello");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("hello", post.content());
 }
