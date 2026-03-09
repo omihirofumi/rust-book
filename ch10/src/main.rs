eb3ffe56e1557217a34fee09fb82165add9a01a5
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-09 21:11:57 +0900

    trait

--- ch10/src/main.rs
+++ ch10/src/main.rs
@@ -1,3 +1,8 @@
+use std::fmt::Display;
+
+// orphan rule
+// -> trait or typeの作者のどっちかがtrait実装に責任を持つ。
+// -> つまり、二つとも外部だとできない。
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -5,7 +10,14 @@ fn main() {
 
     println!("The largest number is {largest}");
 
-    let integer = Point { x: 5, y: 10 };
+    let post = SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you propably already know, people"),
+        reply: false,
+        repost: false,
+    };
+
+    println!("{}", post.summarize());
 }
 
 fn largest_i32(list: &[i32]) -> &i32 {
@@ -64,3 +76,60 @@ struct Point2<T, U> {
     x: T,
     y: U,
 }
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn summarize_author(&self) -> String {
+        String::from("Hello")
+    }
+}
+
+pub struct SocialPost {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub repost: bool,
+}
+
+impl Summary for SocialPost {
+    fn summarize_author(&self) -> String {
+        String::from("Hello")
+    }
+}
+
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// fn notify(item1: &impl Summary, item2: &impl Summary) {}
+// fn notify<T: Summary>(item1: &T, item2: &T) {}
+// fn notify(item: &(impl Summary + Display)) {}
+// fn notify<T: Summary + Display>(item: &T) {}
+
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+// }
+
+// fn returns_summarizable() -> impl Summary {}
