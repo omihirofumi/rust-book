d3cdd457fd7c2b5988c61adb010db6f6e651ee8d
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-10 20:43:12 +0900

    lifetime

---
+++ lifetime/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let result;
+
+    let string2 = String::from("abcdef");
+    result = longest(string1.as_str(), string2.as_str());
+
+    println!("The longest value is {result}");
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
+}
