928b1c15b6e524f714329a954ed197c9ca08ba1c
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-10 21:07:21 +0900

    lifetime inference

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -6,8 +6,34 @@ fn main() {
     result = longest(string1.as_str(), string2.as_str());
 
     println!("The longest value is {result}");
+
+    let novel = String::from("Call me Ishmael. Some years go...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
+}
+
+// fn longest2(x: &str, y: &str) -> &str {
+//     x
+// }
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
