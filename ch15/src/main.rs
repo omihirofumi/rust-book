b94f38382d802149a136b1b7aabd8b9e4b20c836
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-17 20:19:04 +0900

    deref trait

--- ch15/src/main.rs
+++ ch15/src/main.rs
@@ -3,8 +3,33 @@ enum List {
     Nil,
 }
 
+use std::ops::Deref;
+
 use crate::List::{Cons, Nil};
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    // let y = &x;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
