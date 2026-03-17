9f16496f24ae964b57294c894f1b77579c6a2947
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-17 20:43:08 +0900

    Drop trait

--- ch15/src/main.rs
+++ ch15/src/main.rs
@@ -21,6 +21,15 @@ fn main() {
     hello(&m);
     // derefがない世界線
     // hello(&(*m)[..]);
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    drop(c);
+    println!("CustomSmartPointers created");
 }
 
 fn hello(message: &str) {
@@ -40,5 +49,15 @@ impl<T> Deref for MyBox<T> {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}!`", self.data);
     }
 }
