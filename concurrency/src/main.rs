440e4d1c3da0f738fdce65522539edfcd21a3045
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-03-18 21:36:00 +0900

    mpsc

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -24,4 +24,87 @@ fn main() {
     // drop(v);
 
     handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    // let received = rx.recv().unwrap();
+    // println!("Got: {received}");
+    loop {
+        match rx.try_recv() {
+            Ok(val) => {
+                println!("Got: {val}");
+                break;
+            }
+            Err(_) => println!("Never Got]"),
+        }
+    }
+
+    // let (tx, rx) = mpsc::channel();
+
+    // thread::spawn(move || {
+    //     let val = String::from("hi");
+    //     // value moved here
+    //     tx.send(val).unwrap();
+    //     println!("val is {val}");
+    // });
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
